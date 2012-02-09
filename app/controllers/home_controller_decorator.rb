978ad7da9ca52eb284fb94ac66f1f36d0ce58894
seth-macpherson <seth.macpherson@gmail.com>
2012-02-09 11:29:50 -0800

    added domain_ensure method

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -1,5 +1,16 @@
 HomeController.class_eval do
 
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'mylunchin.com'
+
+  def ensure_domain
+    if Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
+
   def about
     
   end
