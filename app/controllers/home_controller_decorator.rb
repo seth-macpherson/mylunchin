a2a190a4b54d1d90f464e1102c8e650eade9542a
seth-macpherson <seth.macpherson@gmail.com>
2012-02-09 12:42:14 -0800

    moved splash and enforced development only from herokuapp.com

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -9,10 +9,11 @@ HomeController.class_eval do
   def ensure_domain
     if SHOW_SPLASH && request.env['HTTP_HOST'] != "mylunchin.herokuapp.com"
       render :splash, :layout => false
-    elsif Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
-      # HTTP 301 is a "permanent" redirect
-      redirect_to "http://#{APP_DOMAIN}", :status => 301
     end
+    # if Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
+    #   # HTTP 301 is a "permanent" redirect
+    #   redirect_to "http://#{APP_DOMAIN}", :status => 301
+    # end
   end
 
   def about
