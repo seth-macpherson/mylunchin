77489827b351b4e84a34eb6df654ebdb6dad907f
seth-macpherson <seth.macpherson@gmail.com>
2012-02-09 10:56:29 -0800

    possibly fixed the precompilation errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,7 @@ Mylunchin::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( extras/*.js )
+  config.assets.precompile += ['store/all.*', 'admin/all.*']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
