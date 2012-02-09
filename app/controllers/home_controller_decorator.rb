c64f1f6c8549e6b4ccbff9cc0b606ab32880e5cd
seth-macpherson <seth.macpherson@gmail.com>
2012-02-09 15:33:55 -0800

    push up latest assets

--- app/controllers/home_controller_decorator.rb
+++ app/controllers/home_controller_decorator.rb
@@ -1,17 +1,17 @@
 HomeController.class_eval do
 
-  #before_filter :ensure_domain
+  before_filter :ensure_domain
 
   APP_DOMAIN = 'www.mylunchin.com'
 
   def ensure_domain
-    if Rails.env.production? && request.env['HTTP_HOST'] != "mylunchin.herokuapp.com"
-      render :splash, :layout => false
-    end
-    # if Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
-    #   # HTTP 301 is a "permanent" redirect
-    #   redirect_to "http://#{APP_DOMAIN}", :status => 301
+    # if Rails.env.production? && request.env['HTTP_HOST'] != "mylunchin.herokuapp.com"
+    #   render :splash, :layout => false
     # end
+    if Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}/?routed", :status => 301
+    end
   end
 
   def about
