755464ad9f35eded053c4590760c056dd6ead976
seth-macpherson <seth.macpherson@gmail.com>
2012-02-09 20:32:50 -0800

    added local mailers for further editing and wrapped the home page's reference to a paperclip attachement in a begin/rescue/end

--- config/initializers/app_configuration.rb
+++ config/initializers/app_configuration.rb
@@ -1,5 +1,3 @@
-# Spree::Config.set(
-#   :admin_interface_logo => "admin/bg/spree_50.png",
-#   :default_country_id => 214, 
-#   :checkout_zone => 'North America'
-# )
+Spree::Config.set(
+  :auto_capture => false
+)
