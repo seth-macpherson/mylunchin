ae32af1e8045d2375010fc581b5f9ce1ee91cd4a
seth-macpherson <seth.macpherson@gmail.com>
2012-02-11 12:44:26 -0800

    added choose_many and fixed the mail methods_controller

--- app/controllers/admin/mail_methods_controller_decorator.rb
+++ app/controllers/admin/mail_methods_controller_decorator.rb
@@ -11,7 +11,7 @@ class Admin::MailMethodsController < Admin::ResourceController
   rescue Exception => e
     flash[:error] = t("admin.mail_methods.testmail.error") % {:e => e}
   ensure
-    respond_with(@mail_method) { |format| format.html { redirect_to :back } }
+    respond_with(@mail_method) { |format| format.html { redirect_to admin_mail_methods_path } }
   end
 
   private
