7d6c4a8c7d876329342ecb7818fef1d94b09a968
seth-macpherson <seth.macpherson@gmail.com>
2012-02-13 00:17:23 -0800

    updated number of admin products per page and patched alt text for image popups

--- config/initializers/app_configuration.rb
+++ config/initializers/app_configuration.rb
@@ -2,6 +2,7 @@ puts "Loading custom app configuration settings"
 Spree::Config.set(
   :auto_capture => true,
   :products_per_page => 50,
-  :orders_per_page => 50
+  :orders_per_page => 50,
+  :admin_products_per_page => 25
 )
 
