f6bedb612542d993d2703a989f626c6402977112
seth-macpherson <seth.macpherson@gmail.com>
2012-03-18 19:12:48 -0700

    added credit card decorator to hopefully provide more meaningful errors on the payment page

---
+++ app/models/credit_card_decorator.rb
@@ -0,0 +1,16 @@
+CreditCard.class_eval do
+
+  def gateway_error(error)
+    if error.is_a? ActiveMerchant::Billing::Response
+      text = error.params['message'] || error.params['response_reason_text'] || error.message
+    elsif error.is_a? ActiveMerchant::ConnectionError
+      text = I18n.t(:unable_to_connect_to_gateway)
+    else
+      text = error.to_s
+    end
+    logger.error(I18n.t('gateway_error'))
+    logger.error("  #{error.to_yaml}")
+    raise Spree::GatewayError.new(text)
+  end
+
+end
